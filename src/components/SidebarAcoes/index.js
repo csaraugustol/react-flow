9a151d269773b9175b0db6590327e83163a83a36
csaraugustol <cesar.leitejm@hotmail.com>
2022-08-17 16:35:34 -0300

    Menu hamburguer

--- src/components/SidebarAcoes/index.js
+++ src/components/SidebarAcoes/index.js
@@ -9,21 +9,31 @@ export default () => {
     event.dataTransfer.effectAllowed = 'move';
   };
 
+  const abrir = document.getElementById('btn-menu');
+  const fechar = document.getElementById('btn-close');
+  const sidebarAcoes = document.getElementById('sidebar-acoes');
+
   return (
     <aside>
-      <h3>Ações</h3>
-          <hr />
-          <h5>Comunicação</h5>
-          <div className="dndnode select-updater" onDragStart={(event) => onDragStart(event, 'sendSms', 'Enviar SMS Node')} draggable>
-              Enviar SMS
-          </div>
-          <div className="dndnode select-updater" onDragStart={(event) => onDragStart(event, 'sendWhatsapp', 'Enviar WhatsApp Node')} draggable>
-              Enviar WhatsApp
-          </div>
-          <div className="dndnode select-updater" onDragStart={(event) => onDragStart(event, 'sendEmail', 'Enviar e-mail Node')} draggable>
-              Enviar e-mail
-          </div>
-          <hr />
+      <button id='btn-menu' className='btn-menu'>Ações</button>
+      <div id='sidebar-acoes' className='sidebar-acoes'>
+        <div className='cabecalho-sidebar-acoes'>
+          <h3>Ações</h3>
+          <button id='btn-close' className='btn-close'></button>
+        </div>
+        <hr />
+        <h5>Comunicação</h5>
+        <div className="dndnode select-updater" onDragStart={(event) => onDragStart(event, 'sendSms', 'Enviar SMS Node')} draggable>
+          Enviar SMS
+        </div>
+        <div className="dndnode select-updater" onDragStart={(event) => onDragStart(event, 'sendWhatsapp', 'Enviar WhatsApp Node')} draggable>
+          Enviar WhatsApp
+        </div>
+        <div className="dndnode select-updater" onDragStart={(event) => onDragStart(event, 'sendEmail', 'Enviar e-mail Node')} draggable>
+          Enviar e-mail
+        </div>
+        <hr />
+      </div>
     </aside>
   );
 };
