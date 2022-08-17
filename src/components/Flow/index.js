92016fcd45c43e99fb9941647638ea2aef3afabf
pedro prado <pedroprado.index@gmail.com>
2022-08-17 09:23:44 -0300

    Horizontal patterns added / "Atraso na coleta" example added

--- src/components/Flow/index.js
+++ src/components/Flow/index.js
@@ -1,12 +1,12 @@
 import ReactFlow, {
-  ReactFlowProvider,
-  addEdge,
-  useNodesState,
-  useEdgesState,
-  Controls,
-  Background,
-  MiniMap,
-  useReactFlow
+    ReactFlowProvider,
+    addEdge,
+    useNodesState,
+    useEdgesState,
+    Controls,
+    Background,
+    MiniMap,
+    useReactFlow
 } from 'react-flow-renderer';
 import './Flow.css';
 import Sidebar from '../Sidebar';
@@ -15,13 +15,15 @@ import SelectUpdaterNode from '../SelectUpdaterNode';
 import React, { useState, useRef, useCallback } from 'react';
 
 const initialNodes = [
-  {
-    id: '1',
-    type: 'input',
-    data: { label: 'Input Node' },
-    position: { x: 250, y: 5 },
-    className: 'base-node input-node-inicial'
-  }
+    {
+        id: '1',
+        type: 'input',
+        data: { label: 'Input Node' },
+        position: { x: 250, y: 5 },
+        sourcePosition: 'right',
+        targetPosition: 'left',
+        className: 'base-node input-node-inicial'
+    }
 ];
 
 // const initialEdges = [
@@ -35,116 +37,119 @@ const getId = () => `dndnode_${id++}`;
 const nodeTypes = { textUpdater: TextUpdaterNode, selectUpdater: SelectUpdaterNode };
 
 const Flow = () => {
-  const reactFlowWrapper = useRef(null);
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-  const [reactFlowInstance, setReactFlowInstance] = useState(null);
-  const { setViewport } = useReactFlow();
+    const reactFlowWrapper = useRef(null);
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+    const [reactFlowInstance, setReactFlowInstance] = useState(null);
+    const { setViewport } = useReactFlow();
 
-  const flowKey = 'flow-token-123';
+    const flowKey = 'flow-token-123';
 
-  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
+    const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
 
-  const onDragOver = useCallback((event) => {
-    event.preventDefault();
-    event.dataTransfer.dropEffect = 'move';
-  }, []);
+    const onDragOver = useCallback((event) => {
+        event.preventDefault();
+        event.dataTransfer.dropEffect = 'move';
+    }, []);
 
-  const onDrop = useCallback(
-    (event) => {
-      event.preventDefault();
+    const onDrop = useCallback(
+        (event) => {
+            event.preventDefault();
 
-      const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
-      const type = event.dataTransfer.getData('application/reactflow');
-      const label = event.dataTransfer.getData('application/label');
+            const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
+            const type = event.dataTransfer.getData('application/reactflow');
+            const label = event.dataTransfer.getData('application/label');
 
-      // check if the dropped element is valid
-      if (typeof type === 'undefined' || !type) {
-        return;
-      }
-      if (typeof label === 'undefined' || !label) {
-        return;
-      }
+            // check if the dropped element is valid
+            if (typeof type === 'undefined' || !type) {
+                return;
+            }
+            if (typeof label === 'undefined' || !label) {
+                return;
+            }
 
-      const position = reactFlowInstance.project({
-        x: event.clientX - reactFlowBounds.left,
-        y: event.clientY - reactFlowBounds.top,
-      });
+            const position = reactFlowInstance.project({
+                x: event.clientX - reactFlowBounds.left,
+                y: event.clientY - reactFlowBounds.top,
+            });
 
-      //const labelSplit = label.split(' ');
-      //const nameOfClass = labelSplit[0]; 
-      //console.log(nameOfClass.toLowerCase());
-      const nameOfClass = label.replace(" ", "-")
-      const newNode = {
-        id: getId(),
-        type,
-        position,
-        data: { label: `${label}` },
-        className: 'base-node ' + nameOfClass.toLowerCase(),
-        //className: 'dndnode ' + nameOfClass.toLowerCase(),
-      };
+            //const labelSplit = label.split(' ');
+            //const nameOfClass = labelSplit[0]; 
+            //console.log(nameOfClass.toLowerCase());
+            const nameOfClass = label.replace(" ", "-")
+            const newNode = {
+                id: getId(),
+                type,
+                position,
+                sourcePosition: 'right',
+                targetPosition: 'left',
+                data: { label: `${label}` },
+                className: 'base-node ' + nameOfClass.toLowerCase(),
+                //className: 'dndnode ' + nameOfClass.toLowerCase(),
+            };
 
-      setNodes((nds) => nds.concat(newNode));
-    },
-    [reactFlowInstance]
-  );
+            setNodes((nds) => nds.concat(newNode));
+        },
+        [reactFlowInstance]
+    );
 
-  const onSave = useCallback(() => {
-    if (reactFlowInstance) {
-      const flow = reactFlowInstance.toObject();
-      localStorage.setItem(flowKey, JSON.stringify(flow));
-    }
-  }, [reactFlowInstance]);
+    const onSave = useCallback(() => {
+        if (reactFlowInstance) {
+            const flow = reactFlowInstance.toObject();
+            localStorage.setItem(flowKey, JSON.stringify(flow));
+        }
+    }, [reactFlowInstance]);
 
-  const onRestore = useCallback(() => {
-    const restoreFlow = async () => {
-      const flow = JSON.parse(localStorage.getItem(flowKey));
+    const onRestore = useCallback(() => {
+        const restoreFlow = async () => {
+            const flow = JSON.parse(localStorage.getItem(flowKey));
 
-      if (flow) {
-        const { x = 0, y = 0, zoom = 1 } = flow.viewport;
-        setNodes(flow.nodes || []);
-        setEdges(flow.edges || []);
-        setViewport({ x, y, zoom });
-      }
-    };
+            if (flow) {
+                const { x = 0, y = 0, zoom = 1 } = flow.viewport;
+                setNodes(flow.nodes || []);
+                setEdges(flow.edges || []);
+                setViewport({ x, y, zoom });
+            }
+        };
 
-    restoreFlow();
-  }, [setNodes, setViewport]);
+        restoreFlow();
+    }, [setNodes, setViewport]);
 
-  return (
-    <div className='dndflow'>
-      <ReactFlowProvider>
-        <div className="reactflow-wrapper" ref={reactFlowWrapper}>
-          <ReactFlow
-            nodes={nodes}
-            edges={edges}
-            onNodesChange={onNodesChange}
-            onEdgesChange={onEdgesChange}
-            onConnect={onConnect}
-            onInit={setReactFlowInstance}
-            onDrop={onDrop}
-            onDragOver={onDragOver}
-            nodeTypes={nodeTypes}
-            fitView
-          >
-            <Controls />
-            <Background />
-            <MiniMap />
-          </ReactFlow>
+    return (
+        <div className='dndflow'>
+            <ReactFlowProvider>
+                <div className="reactflow-wrapper" ref={reactFlowWrapper}>
+                    <ReactFlow
+                        nodes={nodes}
+                        edges={edges}
+                        onNodesChange={onNodesChange}
+                        onEdgesChange={onEdgesChange}
+                        onConnect={onConnect}
+                        onInit={setReactFlowInstance}
+                        onDrop={onDrop}
+                        onDragOver={onDragOver}
+                        nodeTypes={nodeTypes}
+                        fitView
+                        
+                    >
+                        <Controls />
+                        <Background />
+                        <MiniMap />
+                    </ReactFlow>
+                </div>
+
+                <div className="save__controls">
+                    <button className='btn-save' onClick={onSave}>Save</button>
+                    <button className='btn-restore' onClick={onRestore}>Restore</button>
+                </div>
+                <Sidebar />
+            </ReactFlowProvider>
         </div>
-
-        <div className="save__controls">
-          <button className='btn-save' onClick={onSave}>Save</button>
-          <button className='btn-restore' onClick={onRestore}>Restore</button>
-        </div>
-        <Sidebar />
-      </ReactFlowProvider>
-    </div>
-  );
+    );
 };
 
 export default () => (
-  <ReactFlowProvider>
-    <Flow />
-  </ReactFlowProvider>
+    <ReactFlowProvider>
+        <Flow />
+    </ReactFlowProvider>
 );
